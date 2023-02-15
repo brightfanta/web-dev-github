9d6ffa893b42d448c64bfae856c278ef2df07260
hwanhee Lee <brightfant@gmail.com>
2023-02-15 22:28:15 +0900

    15thFeb23 study commit

--- section12/app.js
+++ section12/app.js
@@ -2,6 +2,14 @@
 
 // document.body.children[1].children[0].href = 'https://google.com';
 
-console.dir(document);
+// console.dir(document);
+
+document.body.children[1].children[0].href = 'https://google.com';
 // alert();
 // window.alert();
+
+let anchorElement = document.getElementById('external-link');
+anchorElement.href = 'https://google.com';
+
+anchorElement = document.querySelector('p a'); // a tag nested in p tag
+anchorElement.href = 'https://naver.com';
---
+++ section12/exercise.js
@@ -0,0 +1,25 @@
+// Exercise Time!
+console.dir(document.body);
+// 1. Select the <h1> element by "drilling into the DOM" and 
+//    save it in a variable with a name of your choice
+let head1 = document.body.children[0];
+console.log(head1);
+// 2. Use the variable from (1) and get access to the "parent"
+//    element of the stored <h1> element (i.e. to the <body> element)
+//    BONUS: Try using the variable from (1) to get access to the 
+//    sibling element (i.e. the <p> element next to the <h1> element)
+console.log(head1.parentElement);
+console.log(head1.nextElementSibling);
+// 3. Select the <h1> element with getElementById and store in
+//    the same or a new variable (up to you)
+head1 = document.getElementById('h1');
+console.log(head1);
+// 4. Select the second <p> element with querySelector (you might
+//    need to add something in the HTML code, e.g. a class) 
+//    and store it in a new variable with a name of your choice
+let paragraph2nd = document.querySelector('.2second-paragraph');
+// 5. BONUS TASK: Try changing the text content of the <p> element
+//    you selected in (4) and set it to any other text of your choice
+console.log(paragraph2nd);
+paragraph2nd.innerText = 'OMG';
+
