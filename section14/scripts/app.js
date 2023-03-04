2b284f22e3493e296a9697d076c3f8065187ce7b
pedalpower <brightfanta@gmail.com>
2023-03-04 17:52:27 +0900

    4thMar23 study commit

---
+++ section14/scripts/app.js
@@ -0,0 +1,15 @@
+const playerConfigOverlayElement = document.getElementById('config-overlay');
+const backDropElement = document.getElementById('backdrop');
+const formElement = document.querySelector('form');
+const errorsOutputElement = document.getElementById('config-errors');
+
+const editPlayer1BtnElement = document.getElementById('eidt-player-1-btn');
+const editPlayer2BtnElement = document.getElementById('eidt-player-2-btn');
+const cancelConfigBtnElement = document.getElementById('cancel-config-btn');
+
+editPlayer1BtnElement.addEventListener('click', openPlayerConfig);
+editPlayer2BtnElement.addEventListener('click', openPlayerConfig);
+
+cancelConfigBtnElement.addEventListener('click',closePlayerConfig);
+backDropElement.addEventListener('click', closePlayerConfig);
+formElement.addEventListener('submit',savePlayerConfig);
---
+++ section14/scripts/config.js
@@ -0,0 +1,27 @@
+function openPlayerConfig() {
+  playerConfigOverlayElement.style.display = "block";
+  backDropElement.style.display = "block";
+}
+
+function closePlayerConfig() {
+  playerConfigOverlayElement.style.display = "none";
+  backDropElement.style.display = "none";
+  formElement.firstElementChild.classList.remove("error");
+  errorsOutputElement.textContent = "";
+}
+
+function savePlayerConfig(evt) {
+  evt.preventDefault();
+
+  const formData = new FormData(evt.target); //building object by 'new' instanciating object based on an object blueprint
+
+  const enteredPlayername = formData.get("playername").trim(); //get rid of white space!
+  //   console.log(enteredPlayername);
+
+  if (!enteredPlayername) {
+    // == enteredPlayername === "" treated as false by using '===' don't care about type equality
+    evt.target.firstElementChild.classList.add("error"); // not to override, adding new class
+    errorsOutputElement.textContent = "Please enter a valid name!";
+    return; //return 정의함으로써 if문의 조건이 충족되지 않으면 if문 뒷부분 함수는 실행되지 않는다.
+  }
+}
